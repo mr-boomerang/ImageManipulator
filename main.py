cd3029ace3ad88893fbf6217856ebe8cf92da5b6
mr-boomerang <parths.2410@gmail.com>
2020-05-23 18:53:11 +0530

    Image Timeline Added

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from tkinter import *
 from tkinter.colorchooser import askcolor
 from PIL import ImageTk, Image
 import cv2
+import tkinter.messagebox as messagebox
 
-class Image Manipulator(object):
+class ImageManipulator(object):
 
     DEFAULT_PEN_SIZE = 5.0
     DEFAULT_COLOR = 'black'
+    history = []
+    steps = []
 
     def __init__(self):
         self.root = Tk()
@@ -53,9 +56,17 @@ class Image Manipulator(object):
         self.Grayscale_button = Button(self.root, text='GrayScale', command=self.grayscale)
         self.Grayscale_button.grid(row=4, columnspan=4)
 
+        self.Reset_button = Button(self.root, text='Reset', command=self.reset)
+        self.Reset_button.grid(row=5, columnspan=2)
+
+        self.Previous_button = Button(self.root, text='Prev', command=self.previous)
+        self.Previous_button.grid(row=5, columnspan=2, column = 2)
+
         self.img = cv2.cvtColor(cv2.imread("cameraman.jpg"), cv2.COLOR_BGR2RGB)
         scale_percent = 60 # percent of original size
         self.img = cv2.resize(self.img, (500,500), interpolation = cv2.INTER_AREA)
+        self.history.append(self.img)
+        self.steps.append('OriginalImage')
 
         self.origimg = self.img
         self.photo = ImageTk.PhotoImage(image = Image.fromarray(self.img))
@@ -63,9 +74,14 @@ class Image Manipulator(object):
         self.c = Canvas(self.root, bg='white', width=500, height = 500 )
         self.c.grid(row=1, columnspan=5)
 
-
         # self.c.image = ImageTk.PhotoImage(self.img)
         self.c.create_image(0,0, image = self.photo, anchor = NW)
+
+        self.historyPanel = Text(self.root, state = 'disabled', width = 30)
+        self.historyPanel.grid(row = 0,rowspan = 5,column = 6)
+
+        self.Save_button = Button(self.root, text='Save', command=self.save)
+        self.Save_button.grid(row=5, column = 6)
 
         self.setup()
         self.root.mainloop()
@@ -78,7 +94,10 @@ class Image Manipulator(object):
         self.eraser_on = False
         self.active_button = self.pen_button
         self.c.bind('<B1-Motion>', self.paint)
-        self.c.bind('<ButtonRelease-1>', self.reset)
+        self.c.bind('<ButtonRelease-1>', self.paintreset)
+
+    def paintreset(self, event):
+        self.old_x, self.old_y = None, None
 
     def use_pen(self):
         self.activate_button(self.pen_button)
@@ -113,51 +132,117 @@ class Image Manipulator(object):
         self.img = cv2.GaussianBlur(self.img, (5, 5),0)
         self.photo = ImageTk.PhotoImage(image = Image.fromarray(self.img))
         self.c.create_image(0, 0, image=self.photo, anchor=NW)
+        self.history.append(self.img)
+        self.steps.append('Gaussian Blur')
+        self.printHistory()
 
     def average_blur(self):
         self.img = cv2.blur(self.img, (5,5))
         self.photo = ImageTk.PhotoImage(image = Image.fromarray(self.img))
         self.c.create_image(0, 0, image=self.photo, anchor=NW)
+        self.history.append(self.img)
+        self.steps.append('Average Blur')
+        self.printHistory()
 
     def median_blur(self):
         self.img = cv2.medianBlur(self.img, 5)
         self.photo = ImageTk.PhotoImage(image = Image.fromarray(self.img))
         self.c.create_image(0, 0, image=self.photo, anchor=NW)
+        self.history.append(self.img)
+        self.steps.append('Median Blur')
+        self.printHistory()
 
     def bilateral_filtering(self):
         self.img = cv2.bilateralFilter(self.img,9,75,75)
         self.photo = ImageTk.PhotoImage(image = Image.fromarray(self.img))
         self.c.create_image(0, 0, image=self.photo, anchor=NW)
+        self.history.append(self.img)
+        self.steps.append('Bilateral Filtering')        
+        self.printHistory()
 
     def simple_thresholding(self):
         ret,self.img = cv2.threshold(self.img,127,255,cv2.THRESH_BINARY)
         self.photo = ImageTk.PhotoImage(image = Image.fromarray(self.img))
         self.c.create_image(0, 0, image=self.photo, anchor=NW)
-    
+        self.history.append(self.img)
+        self.steps.append('Simple Threshold')        
+        self.printHistory()
+
     def adaptive_mean(self):
-        self.img = cv2.adaptiveThreshold(self.img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
-        self.photo = ImageTk.PhotoImage(image = Image.fromarray(self.img))
-        self.c.create_image(0, 0, image=self.photo, anchor=NW)
-    
+        try:
+            self.img = cv2.adaptiveThreshold(self.img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
+        except:
+            messagebox.showerror("Error","It only Works on Grayscale Images")
+        else:
+            self.photo = ImageTk.PhotoImage(image = Image.fromarray(self.img))
+            self.c.create_image(0, 0, image=self.photo, anchor=NW)
+            self.history.append(self.img)
+            self.steps.append('Adaptive Mean Thresholding')
+            self.printHistory()
+
+
     def adaptive_gaussian(self):
-        self.img = cv2.adaptiveThreshold(self.img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
-        self.photo = ImageTk.PhotoImage(image = Image.fromarray(self.img))
-        self.c.create_image(0, 0, image=self.photo, anchor=NW)
+        try:
+            self.img = cv2.adaptiveThreshold(self.img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+        except:
+            messagebox.showerror("Error","It only Works on Grayscale Images")
+        else:
+            self.photo = ImageTk.PhotoImage(image = Image.fromarray(self.img))
+            self.c.create_image(0, 0, image=self.photo, anchor=NW)
+            self.history.append(self.img)
+            self.steps.append('Adaptive Gauusian Thresholding')
+            self.printHistory()
 
     def otsu_thresholding(self):
-        ret,self.img = cv2.threshold(self.img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        self.photo = ImageTk.PhotoImage(image = Image.fromarray(self.img))
-        self.c.create_image(0, 0, image=self.photo, anchor=NW)
-        
+        try:
+            ret,self.img = cv2.threshold(self.img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+        except:
+            messagebox.showerror("Error","It only Works on Grayscale Images")
+        else:
+            self.photo = ImageTk.PhotoImage(image = Image.fromarray(self.img))
+            self.c.create_image(0, 0, image=self.photo, anchor=NW)
+            self.history.append(self.img)
+            self.steps.append('Otsu Thresholding')
+            self.printHistory()
+
     def grayscale(self):
         self.img = cv2.cvtColor(self.img, cv2.COLOR_RGB2GRAY)
         self.photo = ImageTk.PhotoImage(image = Image.fromarray(self.img))
         self.c.create_image(0, 0, image=self.photo, anchor=NW)
+        self.history.append(self.img)
+        self.steps.append('Grayscale')
+        self.printHistory()
 
-    def reset(self, event):
-        self.old_x, self.old_y = None, None
+    def reset(self):
+        self.img = self.origimg
+        self.photo = ImageTk.PhotoImage(image = Image.fromarray(self.img))
+        self.c.create_image(0, 0, image=self.photo, anchor=NW)
+        self.history = []
+        self.steps = []
+        self.historyPanel.configure(state='normal')
+        self.historyPanel.delete('1.0',END)
+        self.historyPanel.configure(state='disabled')
 
+    def previous(self):
+        if(len(self.history) > 1):
+            self.history = self.history[:-1]
+            self.steps = self.steps[:-1]
+            self.img = self.history[-1]
+            self.photo = ImageTk.PhotoImage(image = Image.fromarray(self.img))
+            self.c.create_image(0, 0, image=self.photo, anchor=NW)
+            self.printHistory()
+        else:
+            messagebox.showerror("Error","No Previous State Found")
 
+    def save(self):
+        cv2.imwrite('final.jpg',self.img)
+
+    def printHistory(self):
+            self.historyPanel.configure(state='normal')
+            self.historyPanel.delete('1.0',END)
+            for x in self.steps:
+                self.historyPanel.insert('end','\n'+x)
+            self.historyPanel.configure(state='disabled')
 
 if __name__ == '__main__':
     ImageManipulator()
